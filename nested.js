7b2ace8b2975586dca801cee9025a72a4ca77488
KeionT <93876343+KeionT@users.noreply.github.com>
2021-12-01 17:19:46 -0500

    1st commit

---
+++ nested.js
@@ -0,0 +1,24 @@
+let num = 0
+if (num > 0) 
+{   
+    if (num > 100) 
+    {
+        console.log(num + " is greater than 100") 
+    }
+    else if (num==100) 
+    {
+        console.log(num+ " is equal to 100 ")    
+    }
+    else
+    {
+        console.log(num + " is less than 100")
+    }
+}// end of outer if condtion . closing of upper level conditions
+else if( num < 0)
+{
+    console.log(num + " is a negative number")
+}
+else
+{
+    console.log(num + " is zero")
+}
